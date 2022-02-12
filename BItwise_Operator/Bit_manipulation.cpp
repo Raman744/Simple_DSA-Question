3b9bf5663857c57b03998f1b47e781c4eb3aa980
Raman shah <ramanshah744@gmail.com>
2022-02-13 00:22:36 +0530

    12/02/2022

---
+++ BItwise_Operator/Bit_manipulation.cpp
@@ -0,0 +1,20 @@
+// write funtion to
+// -get ith bit
+// -set ith bit
+// -clear ith bit
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getIthBit(int n, int i)
+{
+}
+
+int main()
+{
+    int n = 5;
+    int i;
+    cin >> i;
+    cout << getIthBit(n, i) << endl;
+    return 0;
+}
